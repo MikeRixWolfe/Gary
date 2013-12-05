c8fd291da9de2d8bc13baae4aadeb23d8c0f0a00
MikeRixWolfe <michaelrixwolfe@gmail.com>
2013-12-05 17:18:27 -0600

    fixed gex

--- plugins/gexchange.py
+++ plugins/gexchange.py
@@ -1,18 +1,20 @@
+'''rate exchange from : https://rate-exchange.appspot.com/ '''
+
 from util import hook, http
 
 @hook.command
 def gex(inp):
     '''.gex <from currency> <to currency> <amount> - returns Google Exchange result, <from currency> and <amount> default to USD and 1 respectively if not specified'''
-    ###############################################################
-    ### rate exchange from : https://rate-exchange.appspot.com/ ###
-    ###############################################################
     main_url = 'https://rate-exchange.appspot.com/currency?'
-    query = inp.split(' ', 2)
+    query = inp.decode('utf-8').split(' ', 2)
     if len(query) == 1:
         query.insert(0,'usd')
-    query_url = main_url+'from='+query[0]+'&to='+query[1]
     if len(query) == 2:
-        query.append('1')
+        if query[1].isdigit():
+            query.insert(0,'usd')
+        else:
+            query.append('1')
+    query_url = main_url+'from='+query[0]+'&to='+query[1]
     h = http.get_json(query_url, q=query[2])
     try:
         cur_from = h["from"]
