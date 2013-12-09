026af25db76cf38cf23a2e18bb5f31b25be0e411
MikeRixWolfe <michaelrixwolfe@gmail.com>
2013-12-09 13:05:23 -0600

    Blaze is now Cron, a time based reminder loop; novelty additions to test

---
+++ plugins/cron.py
@@ -0,0 +1,93 @@
+"""
+cron.py: written by MikeFightsBears
+"""
+
+import re
+import time
+import datetime
+from util import hook
+
+timestamp_format = '%I:%M'
+datetimemask = '%Y-%m-%d %H:%M'
+
+class EST(datetime.tzinfo):
+    def utcoffset(self, dt):
+      return datetime.timedelta(hours=-5)
+
+    def dst(self, dt):
+        return datetime.timedelta(0)
+
+
+def localtime(format):
+    return time.strftime(format, time.localtime())
+
+
+def db_init(db):
+    db.execute("create table if not exists cron(time, chan, msg, set_by, recurring,"
+            " primary key(time, chan, set_by))")
+    db.commit()
+
+
+def get_events(db, time, chan):
+    rows = db.execute("select msg, set_by, chan from cron where time=? and chan=lower(?)",
+        (time, chan.lower())).fetchall()
+    if rows:
+        return rows
+    else:
+        return []
+
+
+def set_event(db, time, chan, msg, set_by):
+    db.execute("insert or replace into cron(time, chan, msg, set_by, recurring) values(?,?,?,?,?)", 
+        (time, chan.lower(), msg, set_by, 0))
+    db.commit()
+    return
+
+
+def remove_event(db, time, chan, msg, set_by):
+    db.execute("delete from cron where time=? and chan=lower(?) and msg=? and set_by=?",
+        (time, chan.lower(), msg, set_by))
+    db.commit()
+    return
+
+
+@hook.event('JOIN')
+def cron(inp, say='', chan='', db=None):
+    db_init(db)
+    while True:
+        datestamp = str(datetime.datetime.now(EST()))[:16]
+        rows = get_events(db, datestamp, chan)
+        for row in rows:
+            say("%s: %s" % (row[1], row[0]))
+            remove_event(db, datestamp, row[2], row[0], row[1])
+        time.sleep(30)
+
+
+@hook.event('JOIN')
+def blaze(inp, say=''):
+    while True:
+        timestamp = localtime(timestamp_format)
+        if timestamp == '03:20': # my IRC server is in  a different time zone lol
+            say("4:20 BLAZE IT!")
+        #if str(datetime.datetime.now(EST()))[:16] == 
+        time.sleep(60)
+
+
+@hook.command()
+def remindme(inp, nick='', chan='', db=None):
+    ".remindme YYYY-MM-DD HH:MM <message> - Queues <message> to be output at specified date and time."
+    db_init(db)
+    new_event = re.match(r'(\d\d\d\d-\d\d-\d\d\ \d\d:\d\d)\ (.+)', inp)
+    if new_event and new_event.group(1) > str(datetime.datetime.now(EST()))[:16]:
+        timestamp = new_event.group(1)
+        message = new_event.group(2)
+        
+        try:
+            set_event(db, timestamp, chan, message, nick)
+        except:
+            return "There was an error inserting your event, please try again later."
+        return "Okay, at %s I will remind you of '%s'." % (timestamp, message)
+    elif new_event and new_event.group(1) < str(datetime.datetime.now(EST()))[:16]:
+        return "Please choose a date/time in the future."
+    else:
+        return "Please be sure to use the format 'YYYY-MM-DD HH:MM <message>'."
--- plugins/test.py
+++ plugins/test.py
@@ -4,3 +4,12 @@ from util import hook
 def test(inp, nick='', say=None):
     ".test - tests your hilight window"
     say("Hello %s" % nick)
+
+@hook.command(autohelp=False)
+def kitchensink(inp, nick='', say=None):
+    return "This function is still in development."
+
+@hook.command(autohelp=False)
+def mulched(inp, nick='', say=None):
+    say("Get mulched %s." % nick)
+
