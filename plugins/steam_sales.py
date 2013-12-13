150bd46e6ad95ecc219b781e44637332e199b141
MikeRixWolfe <michaelrixwolfe@gmail.com>
2013-12-13 11:08:48 -0600

    Refactored steam_sales

--- plugins/steam_sales.py
+++ plugins/steam_sales.py
@@ -2,139 +2,127 @@
 steam_sales.py - Written by MikeRixWolfe 2013
 """
 import time
-import re
-import random
-import json
-from util import hook, http, text
+from util import hook, http
 from datetime import datetime
 
 running_sale_loops = []
 
-def get_frontpage():
+def get_featured():
     sales_url = "http://store.steampowered.com/api/featured/"
     sales = http.get_json(sales_url)
     return sales
 
 
-def get_sales():
+def get_featuredcategories():
     sales_url = "http://store.steampowered.com/api/featuredcategories/"
     sales = http.get_json(sales_url)
     return sales
+    
 
+def get_sales(mask_items):
+    # Fetch data
+    try:
+        data = get_featuredcategories()
+        flash_data = get_featured()
+    except:
+        return {}
+        
+    # Aggregate data
+    data["flash"] = {}
+    data["flash"]["name"] = "Flash Sales"
+    data["flash"]["items"] = flash_data["large_capsules"]
 
+    # Mask Data
+    for item in mask_items:
+        del data[item]
+        
+    # Format data
+    sales = {}
+    for category in data:
+        for item in data[category]["items"]:
+            if "url" in item.keys() and item["url"] != "":
+                data[category]["name"] = item["name"]
+                appid = str(item["url"])[34:-1]
+                appdata = http.get_json("http://store.steampowered.com/api/appdetails/?appids=%s" % appid)
+                item["name"] = appdata[appid]["data"]["name"]
+                item["id"] = appdata[appid]["data"]["steam_appid"]
+                item["final_price"] = appdata[appid]["data"]["price_overview"]["final"]
+                item["discounted"] = True
+                item["discount_percent"]  = appdata[appid]["data"]["price_overview"]["discount_percent"]
+            if item["discounted"]:
+                item["id"] = str(item["id"])
+                if data[category]["name"] in sales.keys():
+                    sales[data[category]["name"]].append(item)
+                else:
+                    sales[data[category]["name"]] = []
+                    sales[data[category]["name"]].append(item)
+        sales[data[category]["name"]] = sorted(sales[data[category]["name"]], key=lambda k: k["name"])
+    
+    # Return usable data
+    return sales
+
+    
 @hook.command()
 def steamsales(inp, say=''):
-        ".steamsales <flash|specials|top_sellers|daily|all> - Check Steam for specified sales; Displays special event deals on top of chosen deals."
-        options={"flash": "Flash Sales", "specials" : "Specials", "top_sellers" : "Top Sellers", "daily" : "Daily Deal", "all" : "All"}
+    ".steamsales <flash|specials|top_sellers|daily|all> - Check Steam for specified sales; Displays special event deals on top of chosen deals."
+    options={"flash": "Flash Sales", "specials" : "Specials", "top_sellers" : "Top Sellers", "daily" : "Daily Deal", "all" : "All"}
+
+    # Verify and stage input data
+    inp = inp.lower().split()
+    inp = [line.strip(', ') for line in inp]
+    for i in inp:
+        if i not in options.keys():
+            inp.remove(i)
     
-        # Verify and stage input data
-        inp = inp.lower().split()
-        inp = [line.strip(', ') for line in inp]
-        for i in inp:
-            if i not in options.keys():
-                inp.remove(i)        
+    # Check for bad input
+    if inp == []: 
+        return steamsales.__doc__
 
-        # Get store data
-        data = get_sales()
-        flash_data = get_frontpage()
-        data["flash"] = {}
-        data["flash"]["name"] = "Flash Sales"
-        data["flash"]["items"] = flash_data["large_capsules"]
+    # Get data
+    mask = ["coming_soon","new_releases","genres","trailerslideshow","status"]
+    sales = get_sales(mask)
+    
+    # Mask data for users request
+    if "all" not in inp:
+        for key in options:
+            if key not in inp and options[key] in sales.keys():
+                del sales[options[key]]
 
-        # Clean trash data
-        del data["coming_soon"], data["new_releases"], data["genres"], data["trailerslideshow"], data["status"]
-
-        # Format data
-        sales = {}
-        for category in data:
-            for item in data[category]["items"]:
-                if "url" in item.keys() and item["url"] != "":
-                    data[category]["name"] = item["name"]
-                    appid = str(item["url"])[34:-1]
-                    appdata = http.get_json("http://store.steampowered.com/api/appdetails/?appids=%s" % appid)
-                    item["name"] = appdata[appid]["data"]["name"]
-                    item["final_price"] = appdata[appid]["data"]["price_overview"]["final"]
-                    item["discounted"] = True
-                    item["discount_percent"]  = appdata[appid]["data"]["price_overview"]["discount_percent"]
-                if item["discounted"]:
-                    if data[category]["name"] in sales.keys():
-                        sales[data[category]["name"]].append(item)
-                    else:
-                        sales[data[category]["name"]] = []
-                        sales[data[category]["name"]].append(item)
-            sales[data[category]["name"]] = sorted(sales[data[category]["name"]], key=lambda k: k["name"])
-        if "all" not in inp:
-            for key in options:
-                if key not in inp and options[key] in sales.keys():
-                    del sales[options[key]]
-
-        # Check for bad input
-        if inp == []:
-            return steamsales.__doc__
-
-        # Output appropriate data
-        for category in sales:
-            message = ""
-            for item in sales[category]:
-                if message == "":
-                    message = "\x02" + category + "\x0F: "
-                message += "\x02%s\x0F: $%s.%s(%s%% off)" % \
-                    (item["name"],
-                    str(item["final_price"])[:-2],
-                    str(item["final_price"])[-2:],
-                    str(item["discount_percent"]))
-                message += "; "
-            message = message.strip(':; ')
-            if message != "\x02" + category + "\x0F":
-                say(message)
-            else:
-                say("%s: None found" % message)
-
-
+    # Output appropriate data
+    for category in sales:
+        message = ""
+        for item in sales[category]:
+            if message == "":
+                message = "\x02" + category + "\x0F: "
+            message += "\x02%s\x0F: $%s.%s(%s%% off)" % \
+                (item["name"],
+                str(item["final_price"])[:-2],
+                str(item["final_price"])[-2:],
+                str(item["discount_percent"]))
+            message += "; "
+        message = message.strip(':; ')
+        if message != "\x02" + category + "\x0F":
+            say(message)
+        else:
+            say("%s: None found" % message)
+    
+    
 @hook.event('JOIN')
 def saleloop(inp, say='', chan=''):
     # Don't spawn threads for private messages
     global running_sale_loops
     if chan[0] != '#' or chan in running_sale_loops:
-        return 
+        return
     running_sale_loops.append(chan)
     prev_sales = {}
 
     while True:
         print(">>> u'Beginning check for new Steam sales :%s'" % chan)
 
-        # Fetch data
-        data = get_sales()
-        flash_data = get_frontpage()
-        data["flash"] = {}
-        data["flash"]["name"] = "Flash Sales"
-        data["flash"]["items"] = flash_data["large_capsules"]
-        
-        # Mask data
-        del data["specials"], data["coming_soon"], data["top_sellers"], data["new_releases"], data["genres"], data["trailerslideshow"], data["status"]
+        # Get data
+        mask = ["specials","coming_soon","top_sellers","new_releases","genres","trailerslideshow","status"]
+        sales = get_sales(mask)
 
-        # Format data
-        sales = {}
-        for category in data:
-            for item in data[category]["items"]:
-                if "url" in item.keys() and item["url"] != "":
-                    data[category]["name"] = item["name"]
-                    appid = str(item["url"])[34:-1]
-                    appdata = http.get_json("http://store.steampowered.com/api/appdetails/?appids=%s" % appid)
-                    item["name"] = appdata[appid]["data"]["name"]
-                    item["id"] = appdata[appid]["data"]["steam_appid"]
-                    item["final_price"] = appdata[appid]["data"]["price_overview"]["final"]
-                    item["discounted"] = True
-                    item["discount_percent"]  = appdata[appid]["data"]["price_overview"]["discount_percent"]
-                if item["discounted"]:
-                    item["id"] = str(item["id"])
-                    if data[category]["name"] in sales.keys():
-                        sales[data[category]["name"]].append(item)
-                    else:
-                        sales[data[category]["name"]] = []
-                        sales[data[category]["name"]].append(item)
-            sales[data[category]["name"]] = sorted(sales[data[category]["name"]], key=lambda k: k["name"]) 
-        
         # Cut down on spam on bot restarts
         if prev_sales == {}:
             prev_sales = sales
