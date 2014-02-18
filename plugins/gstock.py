51616579f70b6820add77bbdbb2054eb06bf24e2
MikeRixWolfe <michaelrixwolfe@gmail.com>
2014-02-18 12:46:15 -0600

    Stocks: seperating yahoo and google, split yahoo calls into methods, added stock download, removed stock library

--- plugins/gstock.py
+++ plugins/gstock.py
@@ -1,7 +1,23 @@
-from util import hook, stock
+import json
+from util import hook, http
 
+
+def get_stock(symbol):
+    url = "http://finance.google.com/finance/info?client=ig&format=json&q="
+
+    try:
+        quote = json.loads(http.get(url + symbol)[3:])[0]
+    except:
+        return None
+
+    if quote['c'][0] == '-':
+        quote['color'] = "5"
+    else:
+        quote['color'] = "3"
+
+    return "%(t)s - $%(l_cur)s \x03%(color)s%(c)s (%(cp)s)\x03 [%(lt)s]" % quote
 
 @hook.command()
 def gstock(inp, say=None):
-    ".gstock <symbol> / .gstock <exchange>:<symbol> - Returns the current value of a given stock, default market is NASDAQ. Automatically searches for symbols given company names."
-    say(stock.getstock(inp) or "Google Finance API error, please try again in a few minutes")
+    '''.gstock <symbol> - Gets stock information from Google.'''
+    say(get_stock(inp) or "Google Finance API error, please try again in a few minutes.")
--- plugins/ystock.py
+++ plugins/ystock.py
@@ -5,60 +5,85 @@ ystock.py - rewritten by MikeFightsBears 2013
 import random
 from datetime import date, timedelta
 from util import hook, http, web
-from util import stock as gstock
+
+
+def get_stock_download(inp):
+    try:
+        url = 'http://query.yahooapis.com/v1/public/yql?format=json'
+        q = "select * from csv where url='http://download.finance.yahoo.com/d/quotes.csv?s=%s&f=sl1d1t1c1ohgv&e=.csv' " \
+            "and columns='Name,LastTradePriceOnly,Date,LastTradeTime,Change,Open,DaysHigh,DaysLow,Volume'" % inp
+        query = http.get_json(url, q = q).get('query', '')
+        quote = query.get('results', '').get('row', '')
+        quote['PercentChange'] = "%.2f" % (float(quote['Change']) / float(quote['Open']) * 100)
+    except:
+        return None
+
+    return quote
+
+
+def get_stock_rest(inp, q='SELECT * FROM yahoo.finance.quotes WHERE symbol in ("%s")'):
+    try:
+        url = 'http://query.yahooapis.com/v1/public/yql?' \
+            'format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+        q = q % inp
+        query = http.get_json(url, q = q, timeout = 15).get('query', '')
+        quote = query.get('results', '').get('quote', '')
+    except:
+        return None
+
+    return quote
+
+
+def get_stock_console(inp, q="SELECT * FROM yahoo.finance.quotes WHERE symbol=@symbol LIMIT 1"):
+    try:
+        query = web.query(q, {"symbol": inp})
+        quote = query.one()
+    except:
+        return None
+
+    return quote
 
 
 @hook.command
 def stock(inp, say=''):
-    '''.stock <symbol> - gets stock information from Yahoo.'''
-    try:
-        # heh, SQLI
-        query = "SELECT * FROM yahoo.finance.quote WHERE symbol=@symbol LIMIT 1"
-        quote = web.query(query, {"symbol": inp}).one()
-    except:
-        say(gstock.getstock(inp) or "Yahoo Fianance API error, please try again in a few minutes")
-        return
+    '''.stock <symbol> - Gets stock information from Yahoo.'''
+    quote = get_stock_rest(inp) or get_stock_download(inp)
 
-    # if we dont get a company name back, the symbol doesn't match a company
-    if quote['Change'] is None:
-        return "Unknown ticker symbol %s" % inp
+    if not quote:
+        return "Yahoo Fianance API error, please try again in a few minutes."
+    if quote.get('Volume', None) in (None, 'N/A'):
+        return "Unknown ticker symbol '%s'" % inp
 
-    change = float(quote['Change'])
-    price = float(quote['LastTradePriceOnly'])
-
-    if change < 0:
-        quote['color'] = "5"
+    if float(quote['Change']) < 0:
+        quote['Color'] = "5"
     else:
-        quote['color'] = "3"
+        quote['Color'] = "3"
 
-    quote['PercentChange'] = 100 * change / (price - change)
-
-    say ("%(Name)s - $%(LastTradePriceOnly)s "                   \
-          "\x03%(color)s%(Change)s (%(PercentChange).2f%%)\x03 "        \
-          "H: $%(DaysHigh)s L: $%(DaysLow)s " \
-          "MCAP: %(MarketCapitalization)s " \
-          "Volume: %(Volume)s" % quote)
+    say("%(Name)s - $%(LastTradePriceOnly)s " \
+          "\x03%(Color)s%(Change)s (%(PercentChange)s)\x03 " \
+          "H:$%(DaysHigh)s L:$%(DaysLow)s O:$%(Open)s" \
+          "Volume:%(Volume)s [%(LastTradeTime)s]" % quote)
 
 
 @hook.command
 def stockhistory(inp, say=''):
     '''.stockhisory <symbol> - gets stock history information from Yahoo.'''
-    try:
-        query = "SELECT * FROM yahoo.finance.quotes WHERE symbol=@symbol LIMIT 1"
-        quote = web.query(query, {"symbol": inp})
-        out = quote.one()
-    except:
-        say(gstock.getstock(inp) or "Yahoo Fianance API error, please try again in a few minutes")
-        return
+    quote = get_stock_console(inp) or get_stock_rest(inp,
+        'SELECT * FROM yahoo.finance.quotes WHERE symbol in ("%s")')
 
-    if out['PercentChangeFromYearLow'][0] == '-':
-        out['Color'] = "5"
+    if not quote:
+        return "Yahoo Fianance API error, please try again in a few minutes."
+    if quote.get('Volume', None) in (None, 'N/A'):
+        return "Unknown ticker symbol '%s'" % inp
+
+    if float(quote['Change']) < 0:
+        quote['Color'] = "5"
     else:
-        out['Color'] = "3"
+        quote['Color'] = "3"
 
     say("%(Name)s - $%(LastTradePriceOnly)s " \
           "\x03%(Color)s%(ChangeFromYearLow)s (%(PercentChangeFromYearLow)s)\x03 " \
           "Year H: $%(YearHigh)s Year Avg: $%(TwoHundreddayMovingAverage)s " \
           "Year L: $%(YearLow)s; Volume @ %(Volume)s " \
           "(Avg Daily Volume: %(AverageDailyVolume)s) " \
-          "[%(LastTradeTime)s]" % out)
+          "[%(LastTradeTime)s]" % quote)
