dc7197af548057981022a47daa9ffa2805eab460
MikeRixWolfe <michaelrixwolfe@gmail.com>
2014-02-26 12:09:57 -0600

    formatting

--- plugins/bitcoin.py
+++ plugins/bitcoin.py
@@ -4,7 +4,7 @@ from util import http, hook
 @hook.command('btc', autohelp=False)
 @hook.command(autohelp=False)
 def bitcoin(inp, say=None):
-    ".bitcoin - gets current exchange rate for bitcoins from BTC-E"
+    ".bitcoin - Gets current exchange rate for bitcoins from BTC-E"
     data = http.get_json("https://btc-e.com/api/2/btc_usd/ticker")
     ticker = data['ticker']
     say("Current: \x0307${!s}\x0f - High: \x0307${!s}\x0f - Low: \x0307${!s}\x0f - Volume: {!s} BTC".format(
@@ -14,7 +14,7 @@ def bitcoin(inp, say=None):
 @hook.command('ltc', autohelp=False)
 @hook.command(autohelp=False)
 def litecoin(inp, say=None):
-    """litecoin - gets current exchange rate for litecoins from BTC-E"""
+    """litecoin - Gets current exchange rate for litecoins from BTC-E"""
     data = http.get_json("https://btc-e.com/api/2/ltc_usd/ticker")
     ticker = data['ticker']
     say("Current: \x0307${!s}\x0f - High: \x0307${!s}\x0f - Low: \x0307${!s}\x0f - Volume: {!s} LTC".format(
