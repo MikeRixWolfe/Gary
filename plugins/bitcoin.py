5cb6f034c748e3f226c67216ff86bed19e8a6085
MikeRixWolfe <michaelrixwolfe@gmail.com>
2014-02-26 12:08:21 -0600

    rip mt gox

--- plugins/bitcoin.py
+++ plugins/bitcoin.py
@@ -4,17 +4,11 @@ from util import http, hook
 @hook.command('btc', autohelp=False)
 @hook.command(autohelp=False)
 def bitcoin(inp, say=None):
-    ".bitcoin - gets current exchange rate for bitcoins from mtgox"
-    data = http.get_json("https://data.mtgox.com/api/2/BTCUSD/money/ticker")
-    data = data['data']
-    ticker = {
-        'buy': data['buy']['display_short'],
-        'high': data['high']['display_short'],
-        'low': data['low']['display_short'],
-        'vol': data['vol']['display_short'],
-    }
-    say("Current: \x0307%(buy)s\x0f - High: \x0307%(high)s\x0f"
-        " - Low: \x0307%(low)s\x0f - Volume: %(vol)s" % ticker)
+    ".bitcoin - gets current exchange rate for bitcoins from BTC-E"
+    data = http.get_json("https://btc-e.com/api/2/btc_usd/ticker")
+    ticker = data['ticker']
+    say("Current: \x0307${!s}\x0f - High: \x0307${!s}\x0f - Low: \x0307${!s}\x0f - Volume: {!s} BTC".format(
+            ticker['buy'], ticker['high'], ticker['low'], ticker['vol_cur']))
 
 
 @hook.command('ltc', autohelp=False)
