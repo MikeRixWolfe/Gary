d59370abcc0273e7fda748508112bd4bb77dc8f0
MikeRixWolfe <michaelrixwolfe@gmail.com>
2014-03-13 13:29:55 -0500

    attitude

--- plugins/test.py
+++ plugins/test.py
@@ -18,4 +18,4 @@ def kitchensink(inp, nick='', say=None):
 
 @hook.regex(r'^bot roll call')
 def rollcall(inp, chan='', say=None):
-    say("Ah shut up ya dirty shisno")
+    say("Ehh shut up ya dirty shisno")
