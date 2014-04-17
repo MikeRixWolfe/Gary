7dfd342a5094104889383ff2a9daf24d646092f1
MikeRixWolfe <michaelrixwolfe@gmail.com>
2014-04-17 16:13:31 -0500

    first try at markov chaining

---
+++ plugins/markov.py
@@ -0,0 +1,113 @@
+import re
+import random
+import redis as redislib
+from collections import Counter
+from util import hook
+
+chain_length = 2
+max_words = 30
+messages_to_generate = 5
+separator = '<space>'
+stop_word = '<stop>'
+
+redis = redislib.Redis()
+
+
+def get_seeds(message):
+    words = message.split()
+
+    if len(words) > chain_length:
+        words.append(stop_word)
+
+        for i in range(len(words) - chain_length):
+            yield words[i:i + chain_length + 1]
+
+
+def generate_chain(key):
+    gen_words = []
+
+    for i in xrange(max_words):
+        words = key.split(separator)
+        gen_words.append(words[0])
+        next_word = redis.srandmember(key)
+        if not next_word: # or next_word == stop_word:
+            gen_words.append(words[1])
+            break
+
+        key = separator.join(words[1:] + [next_word])
+
+    return (' '.join(gen_words)).replace(' <stop>', '.')
+
+
+def get_message(key):
+    best_message = ''
+
+    for i in range(messages_to_generate):
+        generated = generate_chain(key)
+
+        if len(generated) > len(best_message):
+            best_message = generated
+
+    return best_message or None
+
+
+@hook.event('PRIVMSG')
+def log(paraml, nick='', input=None):
+    if input.msg[0] == '.' or nick == 'extrastout':
+        return
+
+    message = input.msg.lower()
+
+    for words in get_seeds(message):
+        key = separator.join(words[:-1])
+        redis.sadd(key, words[-1])
+
+
+@hook.command(autohelp=False)
+def markov(inp):
+    ".markov [phrase] - Generate a Markov chain randomly or " \
+        "based on a phrase; optional phrase must be > 3 words"
+    messages = []
+
+    if len(inp.split()) > chain_length:
+        for seed in get_seeds(inp.lower()):
+            key = separator.join(seed[:-1])
+            redis.sadd(key, seed[-1])
+            message = get_message(key)
+
+            if message:
+                messages.append(message)
+    else:
+        for i in range(messages_to_generate):
+            message = get_message(redis.randomkey())
+
+            if message:
+                messages.append(message)
+
+    if len(messages):
+        return random.choice(messages)
+
+
+@hook.command(autohelp=False, adminonly=True)
+def redisinfo(inp):
+    ".redisinfo - Gets Redis DB info"
+    info = redis.info()
+    keys = info['db0']['keys']
+    mem = info['used_memory_human']
+    hits = info['keyspace_hits']
+    misses = info['keyspace_misses']
+    commands = info['total_commands_processed']
+    distr = {k: v for k, v in Counter(redis.scard(key) for key in redis.keys()).iteritems()}
+
+    return("I have %s keys (%s); %s processed commands (%s hits/%s misses); "
+        "set distribution by length: %s" % (keys, mem, commands, hits, misses,
+        ', '.join(["%s: %s" % (k, v) for k, v in distr.iteritems()])))
+
+
+@hook.command(autohelp=False, adminonly=True)
+def redisflush(inp):
+    ".redisflush - Flushes Redis DB and resets statistics, use with care"
+    redis.flushall()
+    redis.config_resetstat()
+    return "Redis database flushed."
+
