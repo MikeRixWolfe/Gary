ad90b889f8a58f2a2b3e10b91ecc5e207df4a27c
MikeRixWolfe <michaelrixwolfe@gmail.com>
2014-06-05 14:35:48 -0500

    updated spotify plug

--- plugins/spotify.py
+++ plugins/spotify.py
@@ -1,92 +1,106 @@
-from util import hook, http
-
-import urllib2
-import json
 import re
+from urllib import urlencode
+
+from util import hook, http, web
+
+gateway = 'http://open.spotify.com/{}/{}'  # http spotify gw address
+spuri = 'spotify:{}:{}'
+
+spotify_re = (r'(spotify:(track|album|artist|user):([a-zA-Z0-9]+))', re.I)
+http_re = (r'(open\.spotify\.com\/(track|album|artist|user)\/'
+           '([a-zA-Z0-9]+))', re.I)
 
 
-def get_data(url):
-    headers = {'User-agent':
-               'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13'}
-
-    request = urllib2.Request(url, None, headers)
-    response = urllib2.urlopen(request).read()
-    response = json.loads(response)
-
-    return response
-
-
-def region_filter(data):
-    tracks = data['tracks']
-    track_names = []
-
-    for track in tracks:
-        if("US" in track['album']['availability']['territories'] or "worldwide" in track['album']['availability']['territories']):
-            return track
-
-    return None
+def sptfy(inp, sptfy=False):
+    if sptfy:
+        shortenurl = "http://sptfy.com/index.php"
+        data = urlencode({'longUrl': inp, 'shortUrlDomain': 1, 'submitted': 1, "shortUrlFolder": 6, "customUrl": "",
+                          "shortUrlPassword": "", "shortUrlExpiryDate": "", "shortUrlUses": 0, "shortUrlType": 0})
+        try:
+            soup = http.get_soup(shortenurl, post_data=data, cookies=True)
+        except:
+            return inp
+        try:
+            link = soup.find('div', {'class': 'resultLink'}).text.strip()
+            return link
+        except:
+            message = "Unable to shorten URL: %s" % \
+                      soup.find('div', {'class': 'messagebox_text'}).find('p').text.split("<br/>")[0]
+            return message
+    else:
+        return web.try_googl(inp)
 
 
-@hook.regex(r"(http://open\.spotify\.com/track/\S*)", re.I)
-def spotify_parse(inp, say=None):
-    url = inp.group(0)
-    response = http.get_html(url)
+@hook.command('sptrack')
+@hook.command
+def spotify(inp):
+    """spotify <song> -- Search Spotify for <song>"""
+    try:
+        data = http.get_json("http://ws.spotify.com/search/1/track.json", q=inp.strip())
+    except Exception as e:
+        return "Could not get track information: {}".format(e)
 
-    title_parse = response.xpath("//h1[@itemprop='name']")
-    artist_parse = response.xpath("//h2/a")
-    title = title_parse[0].text_content()
-    artist = artist_parse[0].text_content()
-
-    say("Spotify: %s - %s" % (artist, title))
-
-
-@hook.regex(r"spotify:track:(\S*)", re.I)
-def spotify_parse_uri(inp, say=None):
-    url = "http://open.spotify.com/track/%s" % inp.group(1)
-    response = http.get_html(url)
-
-    title_parse = response.xpath("//h1[@itemprop='name']")
-    artist_parse = response.xpath("//h2/a")
-    title = title_parse[0].text_content()
-    artist = artist_parse[0].text_content()
-
-    say("Spotify: %s - %s" % (artist, title))
+    try:
+        type, id = data["tracks"][0]["href"].split(":")[1:]
+    except IndexError:
+        return "Could not find track."
+    url = sptfy(gateway.format(type, id))
+    return u"\x02{}\x02 by \x02{}\x02 - {}".format(data["tracks"][0]["name"],
+                                                           data["tracks"][0]["artists"][0]["name"], url)
 
 
 @hook.command
-def spotify(inp, say=None):
-    ".spotify <query> - Searches the Spotify database for URL's to any matching songs. Replies with the first result."
+def spalbum(inp):
+    """spalbum <album> -- Search Spotify for <album>"""
+    try:
+        data = http.get_json("http://ws.spotify.com/search/1/album.json", q=inp.strip())
+    except Exception as e:
+        return "Could not get album information: {}".format(e)
 
-    # Put together the request from bot input and the API URL
-    api_url = "http://ws.spotify.com/search/1/track.json?q="
-    parameters = inp.replace(" ", "+")
-    request_url = api_url + parameters
+    try:
+        type, id = data["albums"][0]["href"].split(":")[1:]
+    except IndexError:
+        return "Could not find album."
+    url = sptfy(gateway.format(type, id))
+    return u"\x02{}\x02 by \x02{}\x02 - {}".format(data["albums"][0]["name"],
+                                                           data["albums"][0]["artists"][0]["name"], url)
 
-    data = get_data(request_url)
 
-    # Loop through and find a track that's available in the US
-    track = region_filter(data)
+@hook.command
+def spartist(inp):
+    """spartist <artist> -- Search Spotify for <artist>"""
+    try:
+        data = http.get_json("http://ws.spotify.com/search/1/artist.json", q=inp.strip())
+    except Exception as e:
+        return "Could not get artist information: {}".format(e)
 
-    # Return the track + data, or print 'No Results'
-    if(track):
-        if(len(track['artists']) > 1):
-            artist = "Various Artists"
-        else:
-            artist = track['artists'][0]['name']
+    try:
+        type, id = data["artists"][0]["href"].split(":")[1:]
+    except IndexError:
+        return "Could not find artist."
+    url = sptfy(gateway.format(type, id))
+    return u"\x02{}\x02 - {}".format(data["artists"][0]["name"], url)
 
-        track_name = track['name']
 
-        # Generate HTTP Spotify URL using regex
-        base_url = "http://open.spotify.com/track/"
-
-        find_id = re.compile(r'.*track:(.+)$', re.I)
-        match = find_id.match(track['href'])
-        track_id = match.groups()[0]
-
-        url = base_url + track_id
-
-        say("%s - %s: %s // spotify:track:%s" %
-            (artist, track_name, url, track_id))
-
-    else:
-        return "No Results"
+@hook.regex(*http_re)
+@hook.regex(*spotify_re)
+def spotify_url(match):
+    type = match.group(2)
+    spotify_id = match.group(3)
+    url = spuri.format(type, spotify_id)
+    # no error catching here, if the API is down fail silently
+    data = http.get_json("http://ws.spotify.com/lookup/1/.json", uri=url)
+    if type == "track":
+        name = data["track"]["name"]
+        artist = data["track"]["artists"][0]["name"]
+        album = data["track"]["album"]["name"]
+        return u"Spotify Track: \x02{}\x02 by \x02{}\x02 from the album \x02{}\x02 - {}".format(name, artist,
+                                                                                                        album, sptfy(
+                gateway.format(type, spotify_id)))
+    elif type == "artist":
+        return u"Spotify Artist: \x02{}\x02 - {}".format(data["artist"]["name"],
+                                                                 sptfy(gateway.format(type, spotify_id)))
+    elif type == "album":
+        return u"Spotify Album: \x02{}\x02 - \x02{}\x02 - {}".format(data["album"]["artist"],
+                                                                             data["album"]["name"],
+                                                                             sptfy(gateway.format(type, spotify_id)))
