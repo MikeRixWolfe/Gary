9e588435ec942c84af10eeb509e9a01953ec1cd9
MikeRixWolfe <michaelrixwolfe@gmail.com>
2014-10-31 16:39:35 -0400

    added versions to requirements, moved pyexec to codepad

--- plugins/pyexec.py
+++ plugins/pyexec.py
@@ -1,19 +1,54 @@
-import re, sys, time
+import re
+import sys
+import time
+import urllib
+import urllib2
 from util import hook, http
+from lxml import etree, html
 
-re_lineends = re.compile(r'[\r\n]*')
+
+def get_response(code):
+    url = "http://codepad.org/"
+
+    parameters = {
+        "lang": "Python",
+        "code": code,
+        "run": "True",
+        "submit": "Submit"
+    }
+
+    request = urllib2.Request(url, urllib.urlencode(parameters))
+    response = urllib2.urlopen(request).read()
+    response_url = urllib2.urlopen(request).geturl()
+
+    return [response, response_url]
 
 
+def parse_html(document):
+    html = etree.HTML(document)
+    output = html.xpath('//div[@class="code"][2]//pre//text()')
+    return output
+
+
+@hook.command('py')
 @hook.command
-def python(inp):
-    """.python <prog> - Executes Python code <prog>."""
-    inp = inp.encode('ascii', 'ignore')
+def python(inp, say=None):
+    """.py[thon] <code> - Executes Python code via Codepad.org."""
+
+    code = inp.split(" ")[0:]
+    code = " ".join(code)
+
     try:
-        res = http.get("http://eval.appspot.com/eval", statement=inp).splitlines()
+        response = get_response(code)
+        document, url = response
+        output = parse_html(document)
     except Exception as e:
-        return "Error: {}".format(e)
-    if res:
-        return (re_lineends.split(res[0])[0] if res[0] != 'Traceback (most recent call last):' else res[-1])
+        return e
+
+    if output == []:
+        return "No output"
+
+    return output[-1].encode("ascii", "ignore")
 
 
 @hook.command(adminonly=True)
