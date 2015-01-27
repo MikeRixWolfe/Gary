8d4d3c993b156c66357e6db3f4588f8a0a51de08
MikeRixWolfe <michaelrixwolfe@gmail.com>
2015-01-27 17:16:35 -0500

    Add graphs to google stock

--- plugins/gstock.py
+++ plugins/gstock.py
@@ -1,10 +1,11 @@
 import json
-from util import hook, http
+from util import hook, http, web
 
 
 @hook.command
 def gstock(inp):
     """.gstock <symbol> - Gets stock information from Google."""
+    # Note: NASDAQ:GOOG,NASDAQ:YHOO is a valid example input
     url = "http://finance.google.com/finance/info?client=ig&format=json&q="
 
     try:
@@ -17,5 +18,7 @@ def gstock(inp):
     else:
         quote['color'] = "3"
 
-    return "%(t)s - $%(l_cur)s \x03%(color)s%(c)s (%(cp)s%%)\x03 [%(lt)s]" % quote
+    quote['chart'] = web.try_googl('http://www.google.com/finance/chart?tlf=12&q=' + inp)
+
+    return "%(t)s - $%(l)s \x03%(color)s%(c)s (%(cp)s%%)\x03 - %(chart)s [%(ltt)s]" % quote
 
