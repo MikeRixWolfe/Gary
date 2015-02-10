5390ce7ab1b2e281b7d8f1c2b1c1335a782fe8fb
MikeRixWolfe <michaelrixwolfe@gmail.com>
2015-02-09 23:51:20 -0500

    Fixing formatting and types

--- plugins/exec.py
+++ plugins/exec.py
@@ -1,6 +1,6 @@
 import re
 import subprocess
-#from lxml import etree, html
+from lxml import etree
 from util import hook, http
 
 
@@ -13,7 +13,7 @@ def python(inp, say=None):
     except Exception as e:
         return e
 
-    return re.sub(" +"," "," ".join(out.split(" ")).replace("\n"," ")) or "No output."
+    return re.sub(" +", " ", out.replace("\n", " ")).strip() if out else "No output."
 
 
 @hook.command('rb')
@@ -23,20 +23,20 @@ def ruby(inp, say=None):
     try:
         url = "http://codepad.org/"
         params = {"lang": "Ruby", "code": inp, "run": "True", "submit": "Submit"}
-        html = http.get_xml("http://codepad.org/", query_params=params, get_method="POST")
-        #html = etree.HTML(document)
+        document = http.get("http://codepad.org/", query_params=params, get_method="POST")
+        html = etree.HTML(document)
         out = html.xpath('//div[@class="code"][2]//td[2]//pre//text()')
-
     except Exception as e:
         return e
 
-    return re.sub(" +"," "," ".join(out.split(" ")).replace("\n"," ")) or "No output."
+    return re.sub(" +", " ", " ".join(out).replace("\n", " ")).strip() if out else "No output."
 
 
 @hook.command(adminonly=True)
 def ply(inp):
     """.ply <prog> - Execute local Python."""
     try:
-        return subprocess.check_output(["python", "-c", inp], stderr=subprocess.STDOUT)
+        out = subprocess.check_output(["python", "-c", inp], stderr=subprocess.STDOUT)
+        return re.sub(" +", " ", out.replace("\n", " ")).strip() if out else "No output."
     except subprocess.CalledProcessError as e:
         return e.output
