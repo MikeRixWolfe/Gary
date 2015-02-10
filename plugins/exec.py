166d7543f37d05b1824ede5b7b689010b4d1cb19
MikeRixWolfe <michaelrixwolfe@gmail.com>
2015-02-09 23:30:53 -0500

    Refactored python and ruby execution and put them into one plug

---
+++ plugins/exec.py
@@ -0,0 +1,42 @@
+import re
+import subprocess
+#from lxml import etree, html
+from util import hook, http
+
+
+@hook.command('py')
+@hook.command
+def python(inp, say=None):
+    """.py[thon] <code> - Executes Python code on the Google App Engine."""
+    try:
+        out = http.get("http://eval.appspot.com/eval", statement=inp)
+    except Exception as e:
+        return e
+
+    return re.sub(" +"," "," ".join(out.split(" ")).replace("\n"," ")) or "No output."
+
+
+@hook.command('rb')
+@hook.command
+def ruby(inp, say=None):
+    """.rb/.ruby <code> - Executes Ruby code via Codepad.org."""
+    try:
+        url = "http://codepad.org/"
+        params = {"lang": "Ruby", "code": inp, "run": "True", "submit": "Submit"}
+        html = http.get_xml("http://codepad.org/", query_params=params, get_method="POST")
+        #html = etree.HTML(document)
+        out = html.xpath('//div[@class="code"][2]//td[2]//pre//text()')
+
+    except Exception as e:
+        return e
+
+    return re.sub(" +"," "," ".join(out.split(" ")).replace("\n"," ")) or "No output."
+
+
+@hook.command(adminonly=True)
+def ply(inp):
+    """.ply <prog> - Execute local Python."""
+    try:
+        return subprocess.check_output(["python", "-c", inp], stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        return e.output
