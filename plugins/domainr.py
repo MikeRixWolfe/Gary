37b783c823b3b0ee9d7232f5d1a4d0c3e3985d07
MikeRixWolfe <michaelrixwolfe@gmail.com>
2015-02-18 14:16:32 -0500

    Update for new domainr url style

--- plugins/domainr.py
+++ plugins/domainr.py
@@ -1,11 +1,11 @@
-from util import hook, http
+from util import hook, http, web
 
 
 @hook.command
 def domainr(inp, say=''):
     """.domainr <domain> - Use domai.nr's API to search for a domain, and similar domains."""
     try:
-        data = http.get_json('http://domai.nr/api/json/search?q=' + inp)
+        data = http.get_json('http://domainr.com/api/json/search?q=' + inp)
     except (http.URLError, http.HTTPError) as e:
         return "Unable to get data for some reason. Try again later."
     if data['query'] == "":
@@ -14,4 +14,4 @@ def domainr(inp, say=''):
     for domain in data['results']:
         domains.append(("\x034" if domain['availability'] == "taken" else (
             "\x033" if domain['availability'] == "available" else "\x038")) + domain['domain'] + "\x0f" + domain['path'])
-    say("Domains: {} [http://domai.nr/{}]".format(", ".join(domains), inp.strip()))
+    say("Domains: {} - {}".format(", ".join(domains), web.try_googl("http://domainr.com/?q=" + inp.strip())))
