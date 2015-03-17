562d7487fd936080a2da283d65e642985c5d4f22
MikeRixWolfe <michaelrixwolfe@gmail.com>
2015-03-17 17:48:20 -0400

    Cleaned up message.py, pm's are now handled privately

--- plugins/message.py
+++ plugins/message.py
@@ -1,9 +1,9 @@
 import time
-from util import hook, timesince, text
+from util import hook, text, timesince
 
 
 def db_init(db):
-    "check to see that our db has the tell table and return a dbection."
+    "check to see that our db has the tell table and return a db."
     db.execute("create table if not exists tell"
                "(user_to, user_from, message, chan, time,"
                "primary key(user_to, message))")
@@ -12,28 +12,30 @@ def db_init(db):
     return db
 
 
-def get_tells(db, user_to, chan):
-    return db.execute("select user_from, message, time from tell where"
-                      " user_to=lower(?) and chan=lower(?) order by time",
-                      (user_to.lower(), chan)).fetchall()
+def get_tells(db, recip, chan):
+    return db.execute("select user_from, message, chan, time from tell where"
+                      " user_to=lower(?) and (chan=lower(?) or"
+                      " chan not like '#%') order by time",
+                      (recip.lower(), chan)).fetchall()
 
 
-@hook.event('*')
-def showtells(inp, say='', nick='', chan='', db=None):
+@hook.event('PRIVMSG')
+def showtells(paraml, say='', nick='', chan='', conn=None, db=None):
     db_init(db)
     tells = get_tells(db, nick, chan)
 
-    if not tells:
-        return
-    for tell in tells:
-        user_from, message, time = tell
-        past = timesince.timesince(time)
-        say("%s: Message from %s, %s ago: %s" %
-            (nick, user_from, past, message))  # notice(
-
-    db.execute("delete from tell where user_to=lower(?) and chan=lower(?)",
-               (nick, chan))
-    db.commit()
+    if tells:
+        for tell in tells:
+            if '#' in tell[2]:
+                say("{nick}: Message from {}, {time} ago: {}".format(nick=nick,
+                    time=timesince.timesince(tell[-1]), *tell))
+            else:
+                out = "PM from {}, {time} ago: {}".format(
+                    time=timesince.timesince(tell[-1]), *tell)
+                conn.send("PRIVMSG {} :{}".format(nick, out))
+        db.execute("delete from tell where user_to=lower(?) and (chan=lower(?)"
+                   " or chan not like '#%')",(nick, chan))
+        db.commit()
 
 
 @hook.command('rmsg')
@@ -41,44 +43,31 @@ def showtells(inp, say='', nick='', chan='', db=None):
 @hook.command
 def message(inp, nick='', chan='', conn=None, db=None, input=None, bot=None):
     """.msg/.message <nick> <message> - Relay <message> to <nick> when <nick> is around."""
+    try:
+        recip, msg = inp.lower().split(' ', 1)
+    except:
+        return message.__doc__
 
-    query = inp.split(' ', 1)
-
-    if len(query) != 2:
-        return tell.__doc__
-
-    user_to = query[0].lower()
-    message = query[1].strip()
-    user_from = nick
-
-    if user_to in conn.users.keys():
-        return "%s is currently online, please use /msg instead." % user_to
-
-    if user_to in bot.config['ignored']:
-        return "I've been instructed not to interact with %s." % user_to
+    if recip in conn.users.keys():
+        return "{} is currently online, please use /msg instead.".format(recip)
+    if recip in bot.config['ignored']:
+        return "I've been instructed not to interact with {}.".format(recip)
+    if recip == nick.lower():
+        return "Have you looked in a mirror lately?"
+    if recip == input.conn.nick.lower():
+        return "You need to get your eyes checked."
 
     if input.trigger == 'rmsg':
-        message = text.rainbow(message)
-
-    if chan.lower() == user_from.lower():
-        chan = 'a pm'
-
-    if user_to == user_from.lower():
-        return "Have you looked in a mirror lately?"
-
-    if input.conn.nick.lower() == user_to.lower():
-        # user is looking for us, being a smartass
-        return "You need to get your eyes checked."
+        msg = text.rainbow(msg)
 
     db_init(db)
 
-    if db.execute("select count() from tell where user_to=?",
-                  (user_to,)).fetchone()[0] >= 5:
-        return "That person has too many things queued."
+    if len(get_tells(db, recip, chan)) >= 5:
+        return "{} has too many messages queued.".format(recip)
 
     try:
         db.execute("insert into tell(user_to, user_from, message, chan,"
-                   "time) values(?,?,?,?,?)", (user_to, user_from, message,
+                   "time) values(?,?,?,?,?)", (recip, nick, msg,
                                                chan, time.time()))
         db.commit()
     except db.IntegrityError:
