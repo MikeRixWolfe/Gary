ea88c2ed4da8380507efc73c3e3eb9ef1c7747e0
MikeRixWolfe <michaelrixwolfe@gmail.com>
2015-03-24 13:43:42 -0400

    Updating gex

--- plugins/gexchange.py
+++ plugins/gexchange.py
@@ -4,18 +4,16 @@ from util import hook, http
 
 @hook.command
 def gex(inp):
-    """.gex <amount> <from currency> <to currency> - Returns Google Exchange result; <amount> and <to currency> default to 1 and USD respectively."""
-    inp = re.match(r'(\d+)? ?(\w+)? ?(\w+)?', inp.upper()).groups()
+    """.gex <amount> <from currency> [to] <to currency> - Returns Google Exchange result; <amount> and <to currency> default to 1 and USD respectively."""
+    inp = re.match(r'(\d+)? ?(\w+)?(?: \w{2} | )?(\w+)?', inp.upper()).groups()
     inp = [inp[0] or '1', inp[1] or 'USD', inp[2] or 'USD']
 
     try:
-        data = http.get_json('https://rate-exchange.appspot.com/currency',
-            query_params=dict(zip(['q', 'from', 'to'], inp)))
+        data = http.get_html('https://www.google.com/finance/converter',
+            query_params=dict(zip(['a', 'from', 'to'], inp)))
     except Exception as e:
-        return "Google Exchange API error: {}".format(e)
+        return "Google Finance error: {}".format(e)
 
-    try:
-        return "{} {from} to {to} = {v} (rate: {rate})".format(inp[0], **data)
-    except:
-        return "Error converting {} {} to {}; please check your input.".format(*inp)
+    return ''.join(data.xpath('//div[@id="currency_converter_result"]//text()')).strip() or \
+        "Error converting {} {} to {}; please check your input.".format(*inp)
 
