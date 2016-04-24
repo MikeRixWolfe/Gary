f314ece44af5a46e7b97237da7c15a71b9b8faaa
MikeRixWolfe <michaelrixwolfe@gmail.com>
2016-04-24 14:32:06 -0400

    added hourly weather forecast

--- plugins/wunderground.py
+++ plugins/wunderground.py
@@ -1,6 +1,6 @@
 from util import hook, http
 
-wunder_url = "http://api.wunderground.com/api/{}/forecast/geolookup/conditions/q/{}.json"
+wunder_url = "http://api.wunderground.com/api/{}/{}/geolookup/conditions/q/{}.json"
 cards = {
     0: "N",
     22.5: "NNE",
@@ -31,9 +31,9 @@ def weather(inp, say=None, api_key=None):
         return "Error: API key not set."
 
     try:
-        weather = http.get_json(wunder_url.format(api_key, http.quote(inp)))
+        weather = http.get_json(wunder_url.format(api_key, 'forecast', http.quote(inp)))
         if weather.get('current_observation', None) is None and len(weather['response'].get('results', [])) > 0:
-            weather = http.get_json(wunder_url.format(api_key, weather['response']['results'][0]['zmw']))
+            weather = http.get_json(wunder_url.format(api_key, 'forecast', weather['response']['results'][0]['zmw']))
     except:
         return "Weather Underground API error, please try again in a few minutes."
 
@@ -61,15 +61,41 @@ def forecast(inp, say=None, api_key=None):
         return "Error: API key not set."
 
     try:
-        weather = http.get_json(wunder_url.format(api_key, http.quote_plus(inp)))
+        weather = http.get_json(wunder_url.format(api_key, 'forecast', http.quote_plus(inp)))
         if weather.get('current_observation', None) is None and len(weather['response'].get('results', [])) > 0:
-            weather = http.get_json(wunder_url.format(api_key, weather['response']['results'][0]['zmw']))
+            weather = http.get_json(wunder_url.format(api_key, 'forecast', weather['response']['results'][0]['zmw']))
     except:
         return "Weather Underground API error, please try again in a few minutes."
     try:
         say("\x02{location[city]}, {location[state]}\x0F: ".format(**weather) +
             '; '.join(["\x02{date[weekday]}\x0F: L {low[fahrenheit]}*F, H {high[fahrenheit]}*F, {conditions}".format(**day)
             for day in weather['forecast']['simpleforecast']['forecastday']]))
+    except:
+        try:
+            return "Ambiguous location, please try one of the following: {}".format(
+                ", ".join(["{} {}".format(i['city'], i['state']) for i in weather['response']['results']]))
+        except:
+            return "Error: unable to find weather data for location."
+
+
+@hook.api_key('wunder')
+@hook.command('h')
+@hook.command
+def hourly(inp, say=None, api_key=None):
+    """.h[ourly] <zip code|location> - Gets the 12 hour weather forecast."""
+    if api_key is None:
+        return "Error: API key not set."
+
+    try:
+        weather = http.get_json(wunder_url.format(api_key, 'hourly', http.quote_plus(inp)))
+        if weather.get('hourly_forecast', None) is None and len(weather['response'].get('results', [])) > 0:
+            weather = http.get_json(wunder_url.format(api_key, 'hourly', weather['response']['results'][0]['zmw']))
+    except:
+        return "Weather Underground API error, please try again in a few minutes."
+    try:
+        say("\x02{location[city]}, {location[state]}\x0F: ".format(**weather) +
+            '; '.join(["\x02{FCTTIME[civil]}\x0F: {temp[english]}*F, {condition}".format(**day)
+            for day in weather['hourly_forecast'][:12]]))
     except:
         try:
             return "Ambiguous location, please try one of the following: {}".format(
