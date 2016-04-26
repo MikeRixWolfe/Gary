53b4c4d0526bce321591193a371c8a70563dd186
MikeRixWolfe <michaelrixwolfe@gmail.com>
2016-04-25 22:35:56 -0400

    Fixed zmw fallback url, added severe weather alerts to .weather

--- plugins/wunderground.py
+++ plugins/wunderground.py
@@ -1,6 +1,6 @@
 from util import hook, http
 
-wunder_url = "http://api.wunderground.com/api/{}/{}/geolookup/conditions/q/{}.json"
+wunder_url = "http://api.wunderground.com/api/{}/{}/q/{}.json"
 cards = {
     0: "N",
     22.5: "NNE",
@@ -21,6 +21,24 @@ cards = {
     360: "N"
 }
 
+alerts = {
+    'HUR': 'Hurricane Local Statement',
+    'TOR': 'Tornado Warning',
+    'TOW': 'Tornado Watch',
+    'WRN': 'Severe Thunderstorm Warning',
+    'SEW': 'Severe Thunderstorm Watch',
+    'WIN': 'Winter Weather Advisory',
+    'FLO': 'Flood Warning',
+    'WAT': 'Flood Watch / Statement',
+    'WND': 'High Wind Advisory',
+    'SVR': 'Severe Weather Statement',
+    'HEA': 'Heat Advisory',
+    'FOG': 'Dense Fog Advisory',
+    'FIR': 'Fire Weather Advisory',
+    'VOL': 'Volcanic Activity Statement',
+    'HWW': 'Hurricane Wind Warning'
+}
+
 
 @hook.api_key('wunder')
 @hook.command('w')
@@ -31,19 +49,25 @@ def weather(inp, say=None, api_key=None):
         return "Error: API key not set."
 
     try:
-        weather = http.get_json(wunder_url.format(api_key, 'forecast', http.quote(inp)))
+        weather = http.get_json(wunder_url.format(api_key, 'forecast/geolookup/conditions', http.quote(inp)))
+        alert = http.get_json(wunder_url.format(api_key, 'alerts', http.quote(inp)))
         if weather.get('current_observation', None) is None and len(weather['response'].get('results', [])) > 0:
-            weather = http.get_json(wunder_url.format(api_key, 'forecast', weather['response']['results'][0]['zmw']))
+            weather = http.get_json(wunder_url.format(api_key,
+                'forecast/geolookup/conditions', 'zmw:' + weather['response']['results'][0]['zmw']))
+            alert = http.get_json(wunder_url.format(api_key, 'alerts', 'zmw:' + alert['response']['results'][0]['zmw']))
     except:
         return "Weather Underground API error, please try again in a few minutes."
 
     try:
         direction = cards.get(float(weather['current_observation']['wind_degrees']),
             cards[min(cards.keys(), key=lambda k: abs(k - float(weather['current_observation']['wind_degrees'])))])
+        alert = [a for a in alert['alerts'] if a['type'] in alerts.keys()]  # Get only alerts we care about
+        alert = "\x02{description}\x0F until {expires}.".format(**alert[0]) if alert else ""
 
         say("\x02{location[city]}, {location[state]}\x0F: {current_observation[temp_f]}*F " \
             "and {current_observation[weather]}, feels like {current_observation[feelslike_f]}*F, " \
-            "wind at {current_observation[wind_mph]} MPH {}, humidity at {current_observation[relative_humidity]}.".format(direction, **weather))
+            "wind at {current_observation[wind_mph]} MPH {}, humidity at {current_observation[relative_humidity]}. " \
+            "{}".format(direction, alert, **weather))
     except:
         try:
             return "Ambiguous location, please try one of the following: {}".format(
@@ -61,9 +85,10 @@ def forecast(inp, say=None, api_key=None):
         return "Error: API key not set."
 
     try:
-        weather = http.get_json(wunder_url.format(api_key, 'forecast', http.quote_plus(inp)))
+        weather = http.get_json(wunder_url.format(api_key, 'forecast/geolookup/conditions', http.quote_plus(inp)))
         if weather.get('current_observation', None) is None and len(weather['response'].get('results', [])) > 0:
-            weather = http.get_json(wunder_url.format(api_key, 'forecast', weather['response']['results'][0]['zmw']))
+            weather = http.get_json(wunder_url.format(api_key,
+                'forecast/geolookup/conditions', 'zmw:' + weather['response']['results'][0]['zmw']))
     except:
         return "Weather Underground API error, please try again in a few minutes."
     try:
@@ -87,9 +112,10 @@ def hourly(inp, say=None, api_key=None):
         return "Error: API key not set."
 
     try:
-        weather = http.get_json(wunder_url.format(api_key, 'hourly', http.quote_plus(inp)))
+        weather = http.get_json(wunder_url.format(api_key, 'hourly/geolookup/conditions', http.quote_plus(inp)))
         if weather.get('hourly_forecast', None) is None and len(weather['response'].get('results', [])) > 0:
-            weather = http.get_json(wunder_url.format(api_key, 'hourly', weather['response']['results'][0]['zmw']))
+            weather = http.get_json(wunder_url.format(api_key,
+                'hourly/geolookup/conditions', 'zmw:' + weather['response']['results'][0]['zmw']))
     except:
         return "Weather Underground API error, please try again in a few minutes."
     try:
