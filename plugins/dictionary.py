43ef6fdae61f8f03f4b1ed5e16e9aad3dfd94430
MikeRixWolfe <michaelrixwolfe@gmail.com>
2016-07-13 12:16:07 -0400

    add command hook

--- plugins/dictionary.py
+++ plugins/dictionary.py
@@ -60,6 +60,7 @@ def wordoftheday(inp, say=None, api_key=None):
 
 
 @hook.command('ud')
+@hook.command
 def urban(inp, say=None):
     """.ud <phrase> - Looks up <phrase> on Urban Dictionary."""
     base_url = 'http://api.urbandictionary.com/v0'
