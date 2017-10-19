cc98a39ef4a4fce5c1a01c4b0fecd9b943d90142
MikeRixWolfe <michaelrixwolfe@gmail.com>
2017-10-19 00:43:02 -0400

    ignore audio in links

--- plugins/links.py
+++ plugins/links.py
@@ -38,7 +38,7 @@ def get_info(url):
 
     try:
         mimetype, encoding = mimetypes.guess_type(url)
-        if mimetype and any(mimetype.startswith(t) for t in ['video', 'image']):
+        if mimetype and any(mimetype.startswith(t) for t in ['video', 'audio', 'image']):
             return web.try_googl(url), None
 
         title = http.get_title(url)
