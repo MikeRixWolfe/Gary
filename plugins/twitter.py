014a21f54b4875375d136901871f86c18a978b72
Mike Rix Wolfe <michaelrixwolfe@gmail.com>
2020-03-03 12:53:26 -0600

    oops

--- plugins/twitter.py
+++ plugins/twitter.py
@@ -74,7 +74,7 @@ def twitter(inp, say=None, api_key=None):
             return 'Error: not that many tweets found'
 
     tweet['full_text'] = http.h.unescape(tweet['full_text'])
-    if 0 < len([t.strip() for t in tweet['full_text'].split('\n') if len(t.strip()) > 0]) < 5:
+    if 1 < len([t.strip() for t in tweet['full_text'].split('\n') if len(t.strip()) > 0]) < 5:
         tweet['full_text'] = re.sub(r'(.*?)(https:\/\/t.co\/.*)', r'\1\n\2', tweet['full_text'])
         say(u'{} (@{}) on Twitter:'.format(tweet['user']['name'], tweet['user']['screen_name']))
         for line in [t.strip() for t in tweet['full_text'].split('\n') if len(t.strip()) > 0]:
@@ -93,7 +93,8 @@ def twitter_url(match, say=None, api_key=None):
         tweet = http.get_json(request_url, query_params=params, oauth=True, oauth_keys=api_key)
 
         tweet['full_text'] = http.h.unescape(tweet['full_text'])
-        if 0 < len([t.strip() for t in tweet['full_text'].split('\n') if len(t.strip()) > 0]) < 5:
+        print([tweet['full_text']])
+        if 1 < len([t.strip() for t in tweet['full_text'].split('\n') if len(t.strip()) > 0]) < 5:
             tweet['full_text'] = re.sub(r'(.*?)(https:\/\/t.co\/.*)', r'\1\n\2', tweet['full_text'])
             say(u'{} - {} (@{}) on Twitter:'.format(web.try_googl(match.group(0)),
                 tweet['user']['name'], tweet['user']['screen_name']))
