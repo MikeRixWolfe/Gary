aad566249a07346b3001c05c816859a813cc0005
Mike Rix Wolfe <michaelrixwolfe@gmail.com>
2020-04-20 13:07:54 -0500

    as clean as possible

--- plugins/topic.py
+++ plugins/topic.py
@@ -13,21 +13,15 @@ def ontopic(paraml, nick='', chan=None, conn=None, bot=None):
 
 @hook.command
 def topic(inp, chan=None, conn=None, bot=None):
-    """topic <add|app|del #|set #|ins #> <topic> - Change the topic of a channel. For deletion this may be a #-# range. This is zero indexed."""
+    """topic <add|app|set #|ins #|del #> <topic> - Change the topic of a channel. For deletion this may be a #-# range. This is zero indexed."""
     if chan.startswith('#') and len(inp.split()) > 1:
         _topic = [t for t in bot.config['topics'].get(chan, u'').split(u' | ') if t]
         op, idx0, idx1, clause = re.match(r'^(\S+)(?: (\d)-?(\d)?)?(?: (.*))?$', inp).groups()
 
         if op == 'add' and clause:
-            if len(_topic) == 0:
-                _topic = [clause]
-            else:
-                _topic.insert(0, clause)
+            _topic.insert(0, clause)
         elif op in ['app', 'append'] and clause:
-            if len(_topic) == 0:
-                _topic = [clause]
-            else:
-                _topic.append(clause)
+            _topic.append(clause)
         elif op == 'set' and idx0 and clause:
             _topic[int(idx0)] = clause
         elif op in ['ins', 'insert'] and idx0 and clause:
