3e6b36918bfabcb1af594302e6526325dc7ab004
Mike Rix Wolfe <michaelrixwolfe@gmail.com>
2020-04-20 12:50:22 -0500

    more topic refactoring

--- plugins/topic.py
+++ plugins/topic.py
@@ -15,37 +15,33 @@ def ontopic(paraml, nick='', chan=None, conn=None, bot=None):
 def topic(inp, chan=None, conn=None, bot=None):
     """topic <add|app|del #|set #|ins #> <topic> - Change the topic of a channel. For deletion this may be a #-# range. This is zero indexed."""
     if chan.startswith('#') and len(inp.split()) > 1:
-        _topic = bot.config['topics'].get(chan, u'').split(u' | ')
+        _topic = [t for t in bot.config['topics'].get(chan, u'').split(u' | ') if t]
         op, idx0, idx1, clause = re.match(r'^(\S+)(?: (\d)-?(\d)?)?(?: (.*))?$', inp).groups()
 
         if op == 'add' and clause:
             if len(_topic) == 0:
-                bot.config['topics'][chan] = clause
+                _topic = [clause]
             else:
                 _topic.insert(0, clause)
-                bot.config['topics'][chan] = u' | '.join(_topic)
         elif op in ['app', 'append'] and clause:
             if len(_topic) == 0:
-                bot.config['topics'][chan] = clause
+                _topic = [clause]
             else:
                 _topic.append(clause)
-                bot.config['topics'][chan] = u' | '.join(_topic)
-        elif op in ['del', 'delete'] and idx0:
+        elif op == 'set' and idx0 and clause:
+            _topic[int(idx0)] = clause
+        elif op in ['ins', 'insert'] and idx0 and clause:
+            _topic.insert(int(idx0), clause)
+        elif op in ['del', 'delete'] and int(idx0) <= len(idx1 or idx0) and int(idx1 or idx0) < len(_topic):
             if idx1 is None:
                 _topic.pop(int(idx0))
             else:
                 for i in range(int(idx0), int(idx1) + 1):
                     _topic.pop(int(idx0))
-            bot.config['topics'][chan] = u' | '.join(_topic)
-        elif op == 'set' and idx0 and clause:
-            _topic[int(idx0)] = clause
-            bot.config['topics'][chan] = u' | '.join(_topic)
-        elif op in ['ins', 'insert'] and idx0 and clause:
-            _topic.insert(int(idx0), clause)
-            bot.config['topics'][chan] = u' | '.join(_topic)
         else:
             return "Check your input and try again."
 
+        bot.config['topics'][chan] = u' | '.join(_topic)
         json.dump(bot.config, open('config', 'w'), sort_keys=True, indent=2)
         conn.send(u"TOPIC {} :{}".format(chan, bot.config['topics'][chan]))
 
