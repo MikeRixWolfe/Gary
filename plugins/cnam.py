11e744d519e2cc03d332176ea6bb8a5e871c2522
Mike Rix Wolfe <michaelrixwolfe@gmail.com>
2020-04-28 12:11:53 -0500

    get name from better source

--- plugins/cnam.py
+++ plugins/cnam.py
@@ -5,42 +5,40 @@ from util import hook, http
 @hook.command('cid')
 @hook.command
 def cnam(inp, api_key=None):
-    """cnam <10 digit number> - Get CNAM information for a number via EveryoneAPI."""
-    url = 'https://api.everyoneapi.com/v1/phone/{}'.format(inp)
-
+    """cnam <10 digit number> - Get CNAM information for a number via OpenCNAM & EveryoneAPI."""
     try:
+        url = 'https://api.everyoneapi.com/v1/phone/{}'.format(inp)
         data = http.get_json(url, account_sid=api_key["account_sid"],
             auth_token=api_key["auth_token"],
-            data="name,carrier,location,linetype,cnam")
+            data="carrier,location,linetype")['data']
     except Exception as e:
         return "EveryoneAPI error, please try again in a few minutes."
 
+    try:
+        url = 'https://api.opencnam.com/v3/phone/{}'.format(inp)
+        data2 = http.get_json(url, account_sid=api_key["account_sid"],
+            auth_token=api_key["auth_token"],
+            format="json")
+    except Exception as e:
+        return "OpenCNAM error, please try again in a few minutes."
+
+    data.update(data2)
     out = [u"Caller ID info for {number}".format(**data)]
 
-    if data['data'].get('expanded_name', None) or data['data'].get('cnam', None):
-        if data['data'].get('expanded_name', None):
-            names = [x for x in [data['data'].get('expanded_name', {}).get('first', None),
-                data['data'].get('expanded_name', {}).get('last', None)] if x != None]
-            if names:
-                out.append(u"Name: {}".format(' '.join(names).strip()))
-        else:
-            if data['data']['cnam'].lower() != 'unknown':
-                out.append(u"Name: {}".format(data['data']['cnam']).strip())
+    if data.get('name'):
+        out.append(u"Name: {}".format(data['name']))
 
-    if data.get('type', None) or data['data'].get('linetype', None):
-        types = [x for x in [data.get('type', 'unknown'),
-            data['data'].get('linetype', 'unknown')] if x != 'unknown']
-        if types:
-            out.append(u"Type: {}".format(', '.join(types)))
-
-    if data['data'].get('location', {}).get('city', None) or data['data'].get('location', {}).get('state', None):
-        locs = [x for x in [data['data'].get('location', {}).get('city', None),
-            data['data'].get('location', {}).get('state', None)] if x != None]
+    if data.get('location', {}).get('city') or data.get('location', {}).get('state'):
+        locs = [x for x in [data.get('location', {}).get('city'),
+            data.get('location', {}).get('state')] if x != None]
         if locs:
             out.append(u"Location: {}".format(', '.join(locs)))
 
-    if data['data'].get('carrier', {}).get('name', None):
-        out.append(u"Carrier: {}".format(data['data']['carrier']['name']))
+    if data.get('linetype'):
+        out.append(u"Type: {}".format(data['linetype']))
+
+    if data.get('carrier', {}).get('name'):
+        out.append(u"Carrier: {}".format(data['carrier']['name']))
 
     if len(out) > 1:
         return u"; ".join(out)
