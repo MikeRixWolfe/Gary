d41f388074c8e4a48174516f09fbaee4fdd758e4
Mike Rix Wolfe <michaelrixwolfe@gmail.com>
2020-07-10 12:05:32 -0500

    everyoneapi v1.5 update

--- plugins/cnam.py
+++ plugins/cnam.py
@@ -5,31 +5,20 @@ from util import hook, http
 @hook.command('cid')
 @hook.command
 def cnam(inp, api_key=None):
-    """cnam <10 digit number> - Get CNAM information for a number via OpenCNAM & EveryoneAPI."""
+    """cnam <10 digit number> - Get CNAM information for a number via EveryoneAPI."""
     try:
         url = 'https://api.everyoneapi.com/v1/phone/{}'.format(inp)
         data = http.get_json(url, account_sid=api_key["account_sid"],
             auth_token=api_key["auth_token"],
-            data="carrier,location,linetype")['data']
+            data="name,address,carrier,location,linetype")['data']
     except Exception as e:
         return "EveryoneAPI error, please try again in a few minutes."
 
-    try:
-        url = 'https://api.opencnam.com/v3/phone/{}'.format(inp)
-        data2 = http.get_json(url, account_sid=api_key["account_sid"],
-            auth_token=api_key["auth_token"],
-            format="json")
-    except Exception as e:
-        return "OpenCNAM error, please try again in a few minutes."
+    out = [u"Caller ID info for {}".format(inp)]
 
-    data.update(data2)
-    out = [u"Caller ID info for {number}".format(**data)]
-
-    # name or wire center
     if data.get('name'):
         out.append(u"Name: {}".format(data['name']))
 
-    # rate center
     if data.get('location', {}).get('city') or data.get('location', {}).get('state'):
         locs = [x for x in [data.get('location', {}).get('city'),
             data.get('location', {}).get('state')] if x != None]
